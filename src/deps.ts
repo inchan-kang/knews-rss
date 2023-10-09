80be6345f46060ba0513e6c0fc05d7e295d2e5c3
akngs <jania902@gmail.com>
2023-10-09 15:28:05 +0900

    라이브러리 업데이트

--- src/deps.ts
+++ src/deps.ts
@@ -2,7 +2,7 @@ export { readCSVObjects } from "https://deno.land/x/csv@v0.7.2/mod.ts";
 export {
   type Feed as RawFeed,
   parseFeed,
-} from "https://deno.land/x/rss@0.5.4/mod.ts";
-export * as xml from "https://deno.land/x/xml@2.0.4/mod.ts";
+} from "https://deno.land/x/rss@1.0.0/mod.ts";
+export * as xml from "https://deno.land/x/xml@2.1.1/mod.ts";
 export * as sleep from "https://deno.land/x/sleep@v1.2.1/mod.ts";
 export { groupBy } from "https://deno.land/x/denodash@0.1.3/src/collection/groupBy.ts";
--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
-export { StringReader } from "https://deno.land/std@0.120.0/io/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.203.0/testing/asserts.ts";
+export { StringReader } from "https://deno.land/std@0.203.0/io/mod.ts";
